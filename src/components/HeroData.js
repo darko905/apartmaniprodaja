c9f2b88bc649854d0de468a73a571d348d66a673
Darko <djukicdarko58@gmail.com>
2023-06-06 23:45:23 +0200

    Created Hero components

---
+++ src/components/HeroData.js
@@ -0,0 +1,25 @@
+import Image1 from "../Images/Herobg/apartmani.jpg";
+import Image2 from "../Images/Herobg/contact-form-background.jpg";
+import Image3 from "../Images/Herobg/apartmani1.jpg";
+
+export const HeroData = [
+  {
+    id: 1,
+    image: Image1,
+    title: "Prodaja Luksuznih apartmana Zlatibor",
+    alt: "Image",
+  },
+  {
+    id: 2,
+    image: Image2,
+    title: "Otvoren tip apartmana za potpuni užitak",
+    alt: "Image",
+  },
+  {
+    id: 2,
+    image: Image3,
+    title: "Dovoljno prostora za okupljanje prijatelja i rad u tisini",
+    alt: "Image",
+  },
+  
+];
